985ea1a27515a1ffe364d103003069f4078ee981
Cameron Roberts <cameronroberts1998@gmail.com>
2026-08-16 09:52:52 -0700

    Got Raylib project to build

--- src/App.cpp
+++ src/App.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
+#include "raylib.h"
 
 int main(){
     std::cout << "Hello World!" << "\n";
+
+    const int screenWidth = 500;
+    const int screenHeight = 500;
+
+    InitWindow(screenWidth, screenHeight, "Game");
+
+    SetTargetFPS(60);
+
+    while(!WindowShouldClose()){
+          BeginDrawing();
+            ClearBackground(WHITE);
+            DrawText("HELLOW WORLD!", screenHeight / 2, screenWidth / 2, 20, BLACK);
+
+          EndDrawing();
+
+    }
+
+    CloseWindow();
 }
